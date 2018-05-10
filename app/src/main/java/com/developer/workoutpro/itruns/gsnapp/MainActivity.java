3210fd5755c874ba2b4bb4e749fa5b8e43fffb9b
Jonas Brand <jonasbrand@paderborn.com>
2018-05-10 22:09:32 +0200

    nice

--- app/src/main/java/com/developer/workoutpro/itruns/gsnapp/MainActivity.java
+++ app/src/main/java/com/developer/workoutpro/itruns/gsnapp/MainActivity.java
@@ -1,13 +1,134 @@
 package com.developer.workoutpro.itruns.gsnapp;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Website website = new Website();
+    private String htmlText;
+    private String teil1 [];
+    private String teil2 [];
+    private String datum;
+    private String vertretungen;
+    private String element [];
+    private ArrayList<String> klasse = new ArrayList<>();
+    private ArrayList<String> stunde = new ArrayList<>();
+    private ArrayList<String> vertreter = new ArrayList<>();
+    private ArrayList<String> fach = new ArrayList<>();
+    private ArrayList<String> raum = new ArrayList<>();
+    private ArrayList<String> text = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        website.execute();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                htmlText = website.getLoggedIn();
+
+                getDatum();
+                getVertretungsstunde();
+
+                String ausgabe = datum;
+                for (int index = 1; index < klasse.size(); index++) {
+                    ausgabe = ausgabe + "\n" + klasse.get(index) + stunde.get(index) + vertreter.get(index) + fach.get(index) + raum.get(index) + text.get(index);
+                } // for
+
+                TextView tv = findViewById(R.id.tv);
+                tv.setText(ausgabe);
+            }
+        }, 10000);
+
+    } // Methode onCreate
+
+    private void getDatum() {
+        teil1 = htmlText.split("<div class=\"mon_title\">");
+        teil2 = teil1[1].split(",");
+        datum = teil2[0];
+
+        // Alle Leerstellen vor der ersten Ziffer entfernen
+        while (!((int) datum.charAt(0) > 47 && (int) datum.charAt(0) < 58)) {
+            datum = datum.substring(1);
+        } // while
+    } // Methode getDatum
+
+    private void getVertretungsstunde() {
+        teil1 = htmlText.split("<table class=\"mon_list\">");
+        vertretungen = teil1[1];
+
+        entferne1Klammern();
+        entferne2Klammern();
+
+        while (! (vertretungen.substring(0,7).equals("</table"))) {
+            for (int index = 0; index < 6; index++) {
+                element = vertretungen.split("<");
+
+                // angezeigte Leerzeichen entfernen
+                if (element[0].equals("&nbsp;")) {
+                    element[0] = "";
+                } // if
+
+                switch (index) {
+                    case 0: klasse.add(element[0]); break;
+                    case 1: stunde.add(element[0]); break;
+                    case 2: vertreter.add(element[0]); break;
+                    case 3: fach.add(element[0]); break;
+                    case 4: raum.add(element[0]); break;
+                    case 5: text.add(element[0]); break;
+                } // switch
+
+                entferne2Klammern();
+            } // for
+            entferne2KlammernEnde();
+        } // while
+
+    } // Methode getVertretungsstunde
+
+    private void entferne2Klammern() {
+        while (vertretungen.charAt(0) != '>') {
+            vertretungen = vertretungen.substring(1);
+        } // while
+        vertretungen = vertretungen.substring(1);
+        while (vertretungen.charAt(0) != '>') {
+            vertretungen = vertretungen.substring(1);
+        } // while
+        vertretungen = vertretungen.substring(1);
+    } // Methode enferne2Klammern
+
+    private void entferne2KlammernEnde() {
+        while (vertretungen.charAt(0) != '>') {
+            vertretungen = vertretungen.substring(1);
+        } // while
+        vertretungen = vertretungen.substring(1);
+
+        // dient dem Überprüfen, ob die Tabelle zuende ist
+        while (vertretungen.charAt(0) != '<') {
+            vertretungen = vertretungen.substring(1);
+        } // while
+        if (vertretungen.substring(0, 7).equals("</table")) {
+            return;
+        } // if
+
+        while (vertretungen.charAt(0) != '>') {
+            vertretungen = vertretungen.substring(1);
+        } // while
+        vertretungen = vertretungen.substring(1);
+    } // Methode enferne2KlammernEnde
+
+    private void entferne1Klammern() {
+        while (vertretungen.charAt(0) != '>') {
+            vertretungen = vertretungen.substring(1);
+        } // while
+        vertretungen = vertretungen.substring(1);
+    } // Methode enferne1Klammern
+
 }
---
+++ app/src/main/java/com/developer/workoutpro/itruns/gsnapp/Website.java
@@ -0,0 +1,60 @@
+package com.developer.workoutpro.itruns.gsnapp;
+
+import android.os.AsyncTask;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+
+public class Website extends AsyncTask<Void, Void, String>  {
+
+    private String loginURL;
+    private String heuteURL;
+    private String htmlText;
+    private Document doc;
+
+    @Override
+    protected void onPreExecute() {
+        loginURL = "https://bid.lspb.de/signin/";
+        heuteURL = "https://bid.lspb.de/explorer/ViewDocument/1306999/";
+        htmlText = "";
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        try {
+            Connection.Response loginForm = Jsoup.connect(loginURL)
+                    .method(Connection.Method.GET)
+                    .timeout(10000)
+                    .execute();
+
+            doc = Jsoup.connect(loginURL)
+                    .data("values[login]", "derBenutzername")
+                    .data("values[password]", "dasPasswort")
+                    .data("values[req]", "/")
+                    .cookies(loginForm.cookies())
+                    .post();
+
+            doc = Jsoup.connect(heuteURL)
+                    .cookies(loginForm.cookies())
+                    .post();
+
+            htmlText = doc.outerHtml();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return htmlText;
+    } // Methode doInBackground
+
+    @Override
+    protected void onPostExecute(String string) {
+        htmlText = string;
+    }
+
+    public String getLoggedIn() {
+        return htmlText;
+    } // Methode getLoggedIn
+
+}
