e1dc37c36b9135a2b07601640f275422ec3ca565
Jonas Brand <jonasbrand@paderborn.com>
2018-05-12 01:49:02 +0200

    nice

--- app/src/main/java/com/developer/workoutpro/itruns/gsnapp/MainActivity.java
+++ app/src/main/java/com/developer/workoutpro/itruns/gsnapp/MainActivity.java
@@ -302,6 +302,8 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationView navigationView = findViewById(R.id.nav_view);
 
+        navigationView.getMenu().getItem(0).setChecked(true);
+
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
