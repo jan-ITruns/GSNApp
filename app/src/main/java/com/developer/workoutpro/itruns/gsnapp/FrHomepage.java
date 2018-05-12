a40a480ffcb5bdec282170e6fe6c81c76c50d3b0
Jonas Brand <jonasbrand@paderborn.com>
2018-05-12 22:43:38 +0200

    nice

--- app/src/main/java/com/developer/workoutpro/itruns/gsnapp/FrHomepage.java
+++ app/src/main/java/com/developer/workoutpro/itruns/gsnapp/FrHomepage.java
@@ -2,8 +2,11 @@ package com.developer.workoutpro.itruns.gsnapp;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
@@ -18,6 +21,15 @@ public class FrHomepage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fr_homepage, container, false);
+
+        NavigationView navigation = getActivity().findViewById(R.id.nav_view);
+        Menu drawer_menu = navigation.getMenu();
+        MenuItem menuItem;
+        menuItem = drawer_menu.findItem(R.id.nav_homepage);
+        if(!menuItem.isChecked())
+        {
+            menuItem.setChecked(true);
+        }
 
         webView = view.findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
