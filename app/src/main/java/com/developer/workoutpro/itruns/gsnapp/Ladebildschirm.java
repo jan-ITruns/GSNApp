3d134f2eae0bfbdfc0b5e72cceb7bc2402a71c97
Jonas Brand <jonasbrand@paderborn.com>
2018-05-14 16:07:32 +0200

    nice

--- app/src/main/java/com/developer/workoutpro/itruns/gsnapp/Ladebildschirm.java
+++ app/src/main/java/com/developer/workoutpro/itruns/gsnapp/Ladebildschirm.java
@@ -1,18 +1,28 @@
 package com.developer.workoutpro.itruns.gsnapp;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
 
 public class Ladebildschirm extends AppCompatActivity {
 
     private static int Ladedauer = 6000;
+    private ImageView imgvLadebalken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ladebildschirm);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.ladebalken);
+        imgvLadebalken = findViewById(R.id.imgvLadebalken);
+        imgvLadebalken.startAnimation(animation);
 
         new Handler().postDelayed(new Runnable() {
             @Override
